8863128261192a2eb58d7bfc771831612566e8e2
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2024-05-30 10:31:50 +0200

    :alien: Update moncomptepro test url

--- src/provider/impl/moncomptepro.ts
+++ src/provider/impl/moncomptepro.ts
@@ -5,7 +5,9 @@ import { type Provider } from "../Provider";
 
 const base = (testServer = false): Provider => ({
   getIssuer(pathname = "", params?: Record<string, string>): string {
-    const baseUrl = testServer ? "https://app-test.moncomptepro.beta.gouv.fr" : "https://app.moncomptepro.beta.gouv.fr";
+    const baseUrl = testServer
+      ? "https://app-sandbox.moncomptepro.beta.gouv.fr"
+      : "https://app.moncomptepro.beta.gouv.fr";
     return `${baseUrl}/${pathname.replace(/^\//, "")}${params ? `?${new URLSearchParams(params).toString()}` : ""}`;
   },
 
